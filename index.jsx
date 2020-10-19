f9b519f318d937135bf9bf6e4013c125dfbb413f
Baris Imre <barisimre98@gmail.com>
2020-10-19 12:36:52 +0200

    Connect to database

--- index.jsx
+++ index.jsx
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodeMailer = require('nodemailer');
-const sqlite3 = require('sqlite3');
+const mysql = require('mysql');
 const app = express();
 const config = require('./config')
 const makeEmail = require('./generateEmail')
@@ -11,40 +11,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-let db = new sqlite3.Database('./database.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the database.');
+
+let con = mysql.createConnection({
+    host: "mysql.ia.utwente.nl",
+    user: "tkp2011",
+    password: "R2U5rnVmLSNq9NW8",
+    database: "tkp2011"
 });
 
 app.post('/register', (req, res) => {
-
-    if (!validate(req)) {
-        // TODO: be mad
-    }
+    let errs = [];
 
     // Add participants into database
     // First store users into an array to later insert it into the database
     persons = [];
-    if (req.body.email1 !== "") {
+    if (req.body.name1 !== "") {
         persons.push([req.body.name1, req.body.email1, req.body.number1]);
     }
-    if (req.body.email2 !== "") {
+    if (req.body.name2!== "") {
         persons.push([req.body.name2, req.body.email2, req.body.number2]);
     }
-    if (req.body.email3 !== "") {
+    if (req.body.name3 !== "") {
         persons.push([req.body.name3, req.body.email3, req.body.number3]);
     }
-    // Insert data into database, query based on amount of participants in the team
-    if (persons.length == 1) {
-        db.run("INSERT INTO ParticipantEntries(Name1, Mail1, Number1) VALUES(?,?,?)", [persons[0][0], persons[0][1], persons[0][2]]);
-    } else if (persons.length == 2) {
-        db.run("INSERT INTO ParticipantEntries(Name1, Mail1, Number1, Name2, Mail2, Number2) VALUES(?,?,?, ?,?,?)", [persons[0][0], persons[0][1], persons[0][2], persons[1][0], persons[1][1], persons[1][2]]);
-    } else if (persons.length == 3) {
-      db.run("INSERT INTO ParticipantEntries(Name1, Mail1, Number1, Name2, Mail2, Number2, Name3, Mail3, Number3) VALUES(?,?,?, ?,?,?, ?,?,?)", [persons[0][0], persons[0][1], persons[0][2], persons[1][0], persons[1][1], persons[1][2], persons[2][0], persons[2][1], persons[2][2]]);
+
+    if (!validate(persons, req.body.team)) {
+        console.log('Not valid')
+        res.send('Not valid')
+        return
     }
 
+    con.connect(function (err) {
+        if (err){
+            errs.push('database connection error')
+            console.log(err)
+        }
+        for (person of persons) {
+            var sql = `INSERT INTO participants_2020 (name, email, snumber) VALUES ('${person[0]}', '${person[1]}', '${person[2]}')`;
+            con.query(sql, function (err, result) {
+                if (err) {
+                    if(err.fatal) {
+                        console.log('Fatal error')
+                        res.send('Not valid')
+                        return
+                    }
+                    errs.push('database insert error')
+                    console.log(err)
+                }
+                console.log("1 record inserted");
+            });
+        }
+
+    });
     const tos = [];
 
     if (req.body.email1 !== "") {
@@ -61,42 +79,44 @@ app.post('/register', (req, res) => {
         host: 'smtp.utwente.nl',
         port: 25,
     });
-    console.log(req.body.email1)
 
     const mailOptions = {
         from: 'ICTSV Inter-Actief <tkp@inter-actief.net>',
-        to: tos,
+        to: tos, //TODO add committe email here
         subject: `TAPC participation team: ${req.body.team}`,
         html: makeEmail(req.body.team, [[req.body.name1, req.body.number1, req.body.email1],
-            [req.body.name2, req.body.number2, req.body.email2],
-            [req.body.name2, req.body.number2, req.body.email2]], req.body.official)
+        [req.body.name2, req.body.number2, req.body.email2],
+        [req.body.name2, req.body.number2, req.body.email2]], req.body.official)
     };
 
     console.log([[req.body.name1, req.body.number1, req.body.email1],
-        [req.body.name2, req.body.number2, req.body.email2],
-        [req.body.name2, req.body.number2, req.body.email2]])
+    [req.body.name2, req.body.number2, req.body.email2],
+    [req.body.name2, req.body.number2, req.body.email2]])
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return console.log(error);
+            console.log(error);
+            errs.push('Email error')
         }
-        console.log('Message %s sent: %s', info.messageId, info.response);
     });
+    res.send('good')
 });
-
-
-
-
-
-
-
-
-
 
 
 app.listen(config.port, () => {
     console.log(`Listening at http://localhost:${config.port}`)
 })
 
-function validate(req) {
+function validate(persons, t) {
+    if (t.length === 0) {
+        return false;
+    }
+    if(persons.length === 0){
+        return false;
+    }
+    for(person of persons) {
+        if (person[1] === "" || person[2] === ""){
+            return false;
+        }
+    }
     return true;
 }
