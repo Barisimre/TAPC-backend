54b47095c804b9114494c24cb1471d33c06ce858
Baris Imre <barisimre98@gmail.com>
2020-10-19 12:59:05 +0200

    Add some fixes

--- config.js
+++ config.js
@@ -2,8 +2,22 @@ const config = {};
 
 config.port = 5000;
 
+// Database
 
+config.database = {
+    host: "mysql.ia.utwente.nl",
+    user: "tkp2011",
+    password: "R2U5rnVmLSNq9NW8",
+    database: "tkp2011"
+}
 
+// Email
+
+config.email = {
+    host: 'smtp.utwente.nl',
+    port: 25,
+    from: 'ICTSV Inter-Actief <tkp@inter-actief.net>'
+}
 
 
 
--- index.jsx
+++ index.jsx
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({
 }));
 
 let con = mysql.createConnection({
-    host: "mysql.ia.utwente.nl",
-    user: "tkp2011",
-    password: "R2U5rnVmLSNq9NW8",
-    database: "tkp2011"
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.database
 });
 
 app.post('/register', (req, res) => {
@@ -28,42 +28,22 @@ app.post('/register', (req, res) => {
     if (req.body.name1 !== "") {
         persons.push([req.body.name1, req.body.email1, req.body.number1]);
     }
-    if (req.body.name2!== "") {
+    if (req.body.name2 !== "") {
         persons.push([req.body.name2, req.body.email2, req.body.number2]);
     }
     if (req.body.name3 !== "") {
         persons.push([req.body.name3, req.body.email3, req.body.number3]);
     }
 
+    // Check if data is there. TODO: make better
     if (!validate(persons, req.body.team)) {
         console.log('Not valid')
         res.send('Not valid')
         return
     }
 
-    con.connect(function (err) {
-        if (err){
-            errs.push('database connection error')
-            console.log(err)
-        }
-        for (person of persons) {
-            var sql = `INSERT INTO participants_2020 (name, email, snumber) VALUES ('${person[0]}', '${person[1]}', '${person[2]}')`;
-            con.query(sql, function (err, result) {
-                if (err) {
-                    if(err.fatal) {
-                        console.log('Fatal error')
-                        res.send('Not valid')
-                        return
-                    }
-                    errs.push('database insert error')
-                    console.log(err)
-                }
-                console.log("1 record inserted");
-            });
-        }
-
-    });
-    const tos = [];
+    // Email targets
+    const tos = ["tapc2020@inter-actief.net"];
 
     if (req.body.email1 !== "") {
         tos.push(req.body.email1);
@@ -76,27 +56,62 @@ app.post('/register', (req, res) => {
     }
 
     const transporter = nodeMailer.createTransport({
-        host: 'smtp.utwente.nl',
-        port: 25,
+        host: config.email.host,
+        port: config.email.port,
     });
 
     const mailOptions = {
-        from: 'ICTSV Inter-Actief <tkp@inter-actief.net>',
-        to: tos, //TODO add committe email here
+        from: config.email.from,
+        to: tos,
         subject: `TAPC participation team: ${req.body.team}`,
         html: makeEmail(req.body.team, [[req.body.name1, req.body.number1, req.body.email1],
         [req.body.name2, req.body.number2, req.body.email2],
         [req.body.name2, req.body.number2, req.body.email2]], req.body.official)
     };
-
-    console.log([[req.body.name1, req.body.number1, req.body.email1],
-    [req.body.name2, req.body.number2, req.body.email2],
-    [req.body.name2, req.body.number2, req.body.email2]])
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error);
             errs.push('Email error')
+        } else {
+            console.log('Mail send')
         }
+    });
+
+    // Add people and the team to the database
+    con.connect(function (err) {
+        if (err) {
+            errs.push('database connection error')
+            console.log(err)
+        }
+        for (person of persons) {
+            var sql = `INSERT INTO participants_2020 (name, email, snumber) VALUES ('${person[0]}', '${person[1]}', '${person[2]}')`;
+            con.query(sql, function (err, result) {
+                if (err) {
+                    if (err.fatal) {
+                        console.log('Fatal error')
+                        res.send('Not valid')
+                        return
+                    }
+                    errs.push('database insert error')
+                    console.log(err)
+                }
+                console.log("1 record inserted");
+            });
+        }
+        var sql = `INSERT INTO teams_2020 (name, official_team) VALUES ('${req.body.team}', '${req.body.official ? 1 : 0}')`;
+        con.query(sql, function (err, result) {
+            if (err) {
+                if (err.fatal) {
+                    console.log('Fatal error')
+                    res.send('Not valid')
+                    return
+                }
+                errs.push('database insert error')
+                console.log(err)
+            }
+            console.log("1 record inserted");
+        });
+
     });
     res.send('good')
 });
@@ -110,11 +125,11 @@ function validate(persons, t) {
     if (t.length === 0) {
         return false;
     }
-    if(persons.length === 0){
+    if (persons.length === 0) {
         return false;
     }
-    for(person of persons) {
-        if (person[1] === "" || person[2] === ""){
+    for (person of persons) {
+        if (person[1] === "" || person[2] === "") {
             return false;
         }
     }
